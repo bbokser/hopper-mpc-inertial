Fixed incorrect units, troubleshooting both MPCs for serious stability issues

src/mpc_euler.py
@@ -4,31 +4,26 @@ Copyright (C) 2021-2022 Benjamin Bokser
 
 import numpy as np
 import cvxpy as cp
-from utils import rz, quat2euler
+from utils import rz, hat
 from scipy.linalg import expm
 
 
 class Mpc:
 
-    def __init__(self, t, N, Jinv, rhat, m, g, mu, **kwargs):
+    def __init__(self, t, N, Jinv, rh, m, g, mu, **kwargs):
         n_x = 12  # number of states
         n_u = 6  # number of controls
         self.t = t  # sampling time (s)
         self.N = N  # prediction horizon
         self.Jinv = Jinv
-        self.rhat = rhat
+        self.rh = rh
         self.m = m  # kg
         self.g = g
         self.mu = mu
-        # phi = quat2euler(X_0[3:7])[2]  # extract z-axis euler angle
         A = np.zeros((n_x, n_x))
         A[0:3, 6:9] = np.eye(3)
-        # A[3:6, 9:13] = rz(phi)
         B = np.zeros((n_x, n_u))
         B[6:9, 0:3] = np.eye(3) / self.m
-        # J_w_inv = rz(phi) @ Jinv @ rz(phi).T
-        # B[9:12, 0:3] = J_w_inv @ rhat
-        # B[9:12, 3:6] = J_w_inv
         G = np.zeros((n_x, 1))
         G[8, :] = -self.g
         self.A = A
@@ -37,7 +32,7 @@ class Mpc:
         self.n_x = n_x
         self.n_u = n_u
 
-    def mpcontrol(self, x_in, x_ref_in, C):
+    def mpcontrol(self, x_in, x_ref_in, rf, C):
         x_ref = x_ref_in
         N = self.N
         t = self.t
@@ -48,34 +43,36 @@ class Mpc:
         B = self.B
         G = self.G
         Jinv = self.Jinv
-        rhat = self.rhat
+        rh = self.rh
         n_x = self.n_x
         n_u = self.n_u
+
         x = cp.Variable((N+1, n_x))
         u = cp.Variable((N, n_u))
-        Q = np.eye(n_x)
-        Q[2] *= 0.01
-        R = np.eye(n_u)*0.1
-        cost = 0
-        constr = []
-        u_ref = np.zeros(n_u)
         # TODO: Make Rz_phi a param instead of setting up the problem on every run
         rz_phi = rz(x_in[5])
+        rhat = hat(rh + rz_phi.T @ rf)
         A[3:6, 9:] = rz_phi
         J_w_inv = rz_phi @ Jinv @ rz_phi.T  # world frame Jinv
+        # J_w_inv = np.linalg.inv(rz_phi @ J @ rz_phi.T)  # world frame Jinv
         B[9:12, 0:3] = J_w_inv @ rhat
-        B[9:12, 3:] = J_w_inv
+        B[9:12, 3:] = J_w_inv @ rz_phi.T
         A_bar = np.vstack((np.hstack((A, B, G)), np.zeros((n_u + 1, n_x + n_u + 1))))
-        # I_bar = np.eye(n_x + n_u + 1)
-        # M = I_bar + A_bar * t + 0.5 * (t ** 2) * A_bar @ A_bar
-        M = expm(A_bar * t)
+        I_bar = np.eye(n_x + n_u + 1)
+        M = I_bar + A_bar * t  # + 0.5 * (t ** 2) * A_bar @ A_bar
+        # M = expm(A_bar * t)
         Ad = M[0:n_x, 0:n_x]
         Bd = M[0:n_x, n_x:n_x + n_u]
         Gd = M[0:n_x, -1]
 
+        Q = np.eye(n_x)
+        np.fill_diagonal(Q, [1., 1., 0.5, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01])
+        R = np.eye(n_u)*0
+        # np.fill_diagonal(R, [0.01, 0.01, 0.01, 0.01, 0.01, 0.01])
+        u_ref = np.zeros(n_u)
         # --- calculate cost & constraints --- #
-        np.fill_diagonal(Q, [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.])
-        np.fill_diagonal(R, [0., 0., 0., 0., 0., 0.])
+        cost = 0
+        constr = []
         for k in range(0, N):
             kf = 10 if k == N - 1 else 1  # terminal cost
             kuf = 0 if k == N - 1 else 1  # terminal cost
@@ -86,12 +83,14 @@ class Mpc:
             taux = u[k, 3]
             tauy = u[k, 4]
             tauz = u[k, 5]
+            '''
             constr += [taux <= 20,
                        taux >= -20,
                        tauy <= 20,
                        tauy >= -20,
                        tauz <= 4,
                        tauz >= -4]
+            '''
             if C[k] == 0:  # even
                 u_ref[2] = 0
                 cost += cp.quad_form(x[k + 1, :] - x_ref[k, :], Q * kf) + cp.quad_form(u[k, :] - u_ref, R * kuf)
@@ -107,15 +106,15 @@ class Mpc:
                            0 >= -fx - mu * fz,
                            0 >= fy - mu * fz,
                            0 >= -fy - mu * fz,
-                           fz >= 0,
-                           z <= 3,
+                           fz >= 0,  # TODO: Calculate max vertical force
                            z >= 0.3]
+                           #z <= 3]
 
         constr += [x[0, :] == x_in]  # initial condition
         # constr += [x[-1, :] == x_ref[-1, :]]  # final condition
         # --- set up solver --- #
         problem = cp.Problem(cp.Minimize(cost), constr)
-        problem.solve(solver=cp.ECOS)  # , verbose=True)
+        problem.solve(solver=cp.OSQP)  # , verbose=True)
         if u.value is None:
             raise Exception("\n *** QP FAILED *** \n")
 

src/mpc_euler_cas.py
@@ -6,48 +6,41 @@ import numpy as np
 import casadi as cs
 import itertools
 
-from utils import rz, quat2euler
+from utils import rz, hat
 
 
 class Mpc:
 
-    def __init__(self, t, N, Jinv, rhat, m, g, mu, **kwargs):
+    def __init__(self, t, N, Jinv, rh, m, g, mu, **kwargs):
         n_x = 12  # number of states
         n_u = 6  # number of controls
         self.t = t  # sampling time (s)
         self.N = N  # prediction horizon
         self.Jinv = Jinv
-        self.rhat = rhat
+        self.rhat = rh
         self.m = m  # kg
         self.g = g
         self.mu = mu
 
         A = cs.SX.zeros(n_x, n_x)
         A[0:3, 6:9] = np.eye(3)
-        # phi = quat2euler(X_0[3:7])[2]  # extract z-axis euler angle
-        # rz_phi = rz(phi)
-        # A[3:6, 9:] = rz_phi
         B = cs.SX.zeros(n_x, n_u)
         B[6:9, 0:3] = np.eye(3) / self.m
-        # J_w_inv = rz_phi @ Jinv @ rz_phi.T
-        # B[9:12, 0:3] = J_w_inv @ rhat
-        # B[9:12, 3:6] = J_w_inv
         G = cs.SX.zeros(n_x, 1)
         G[8] = -self.g
 
-        C_in = cs.SX.sym('C', n_x, N)
         rz_phi_in = cs.SX.sym('rz_phi', n_x, 3)
         x_p = cs.SX.sym('x_p', n_x, (N + 1))  # params: x_in, x_ref
         x = cs.SX.sym('x', n_x, (N + 1))  # represents the states over the opt problem.
         u = cs.SX.sym('u', n_u, N)  # decision variables, control action matrix
-
-        C = C_in[0, :]  # awkwardly pull C out of larger array (rest should be zeros)
-
         rz_phi = rz_phi_in[0:3, :].T  # awkwardly pull rz_phi out of larger array (rest should be zeros)
         A[3:6, 9:] = rz_phi
         J_w_inv = rz_phi @ Jinv @ rz_phi.T
+        rf = np.array([0, 0, -0.4])  # body frame foot position TODO: Needs to be a param
+        # rhat = hat(rh + rz_phi.T @ rf)  # TODO: Won't work unless you create a cs.hat
+        rhat = hat(rh + rf)
         B[9:12, 0:3] = J_w_inv @ rhat
-        B[9:12, 3:] = J_w_inv
+        B[9:12, 3:] = J_w_inv @ rz_phi.T
         A_bar = cs.horzcat(cs.horzcat(A, B), G)
         A_bar = cs.vertcat(A_bar, np.zeros((n_u + 1, n_x + n_u + 1)))
         I_bar = np.eye(n_x + n_u + 1)
@@ -74,14 +67,13 @@ class Mpc:
             uk = u[:, k]  # control action
             u_refk = m * g * 2
             # calculate objective
-            obj = obj + cs.mtimes(cs.mtimes((xk - x_refk).T, Q), xk - x_refk) \
-                + cs.mtimes(cs.mtimes((uk - u_refk).T, R), uk - u_refk)
-            dyn = cs.mtimes(Ad, xk) + cs.mtimes(Bd, uk) + Gd
+            obj = obj + (xk - x_refk).T @ Q @ (xk - x_refk) + (uk - u_refk).T @ R @ (uk - u_refk)
+            dyn = Ad @ xk + Bd @ uk + Gd
             constr_dyn = cs.vertcat(constr_dyn, x[:, k + 1] - dyn)  # compute constraints
-            constr_fricx1 = cs.vertcat(constr_fricx1, (uk[0] - mu * uk[2]) * C[k])   # fx - mu*fz
-            constr_fricx2 = cs.vertcat(constr_fricx2, (-uk[0] - mu * uk[2]) * C[k])  # fx - mu*fz
-            constr_fricy1 = cs.vertcat(constr_fricx1, (uk[1] - mu * uk[2]) * C[k])  # fx - mu*fz
-            constr_fricy2 = cs.vertcat(constr_fricx1, (-uk[1] - mu * uk[2]) * C[k])  # fx - mu*fz
+            constr_fricx1 = cs.vertcat(constr_fricx1, (uk[0] - mu * uk[2]))   # fx - mu*fz
+            constr_fricx2 = cs.vertcat(constr_fricx2, (-uk[0] - mu * uk[2]))  # fx - mu*fz
+            constr_fricy1 = cs.vertcat(constr_fricx1, (uk[1] - mu * uk[2]))  # fx - mu*fz
+            constr_fricy2 = cs.vertcat(constr_fricx1, (-uk[1] - mu * uk[2]))  # fx - mu*fz
 
         # append them
         constr = []
@@ -93,7 +85,7 @@ class Mpc:
         constr = cs.vertcat(constr, constr_fricy2)
 
         opt_variables = cs.vertcat(cs.reshape(x, n_x * (N + 1), 1), cs.reshape(u, n_u * N, 1))
-        params = cs.vertcat(x_p.T, rz_phi_in.T, C_in.T)
+        params = cs.vertcat(x_p.T, rz_phi_in.T)
         qp = {'x': opt_variables, 'f': obj, 'g': constr, 'p': params}
         opts = {'print_time': 0, 'error_on_fail': 0, 'printLevel': "none", 'boundTolerance': 1e-6,
                 'terminationTolerance': 1e-6}
@@ -117,7 +109,7 @@ class Mpc:
         self.ubx = ubx
         self.lbx = lbx
 
-    def mpcontrol(self, x_in, x_ref_in, C):
+    def mpcontrol(self, x_in, x_ref_in, rf, C):
         N = self.N
         n_x = self.n_x
         n_u = self.n_u
@@ -131,22 +123,21 @@ class Mpc:
         ubfz = ([200] * N) * C
         lbfx = ([-200] * N) * C
         lbfy = ([-200] * N) * C
-        lbfz = ([0] * N) * C
-
         ubx[(n_x * (N + 1) + 0)::n_u] = ubfx  # upper bound on all f_x
         ubx[(n_x * (N + 1) + 1)::n_u] = ubfy  # upper bound on all f_y
         ubx[(n_x * (N + 1) + 2)::n_u] = ubfz  # upper bound on all f_z
-
         lbx[(n_x * (N + 1) + 0)::n_u] = lbfx  # lower bound on all f_x
         lbx[(n_x * (N + 1) + 1)::n_u] = lbfy  # lower bound on all f_y
+        ubfz = ([400] * N) * C
+        lbfz = ([0] * N)
+        ubx[(n_x * (N + 1) + 2)::n_u] = ubfz  # upper bound on all f_z
         lbx[(n_x * (N + 1) + 2)::n_u] = lbfz  # lower bound on all f_z
 
         u0 = np.zeros((N, n_u))  # six control inputs
         x0_init = np.tile(x_in, (1, N + 1)).T  # initialization of the state's decision variables
 
-        C_in = cs.vertcat(C.reshape(1, -1), np.zeros((n_x-1, N)))
         rz_phi_in = cs.horzcat(rz(x_in[5]), np.zeros((3, n_x-3)))
-        parameters = cs.horzcat(x_in, x_ref_in.T, rz_phi_in.T, C_in).T  # set values of parameters vector
+        parameters = cs.horzcat(x_in, x_ref_in.T, rz_phi_in.T).T  # set values of parameters vector
         # init value of optimization variables
         x0 = cs.vertcat(np.reshape(x0_init.T, (n_x * (N + 1), 1)), np.reshape(u0.T, (n_u * N, 1)))
 

src/robotrunner.py
@@ -18,19 +18,18 @@ class Runner:
     def __init__(self, tool='cvxpy', dyn='euler', dt=1e-3):
         self.dyn = dyn
         self.dt = dt
-        self.total_run = 5000
+        self.total_run = 2000
         # self.tol = 1e-3  # desired mpc tolerance
         self.m = 7.5  # mass of the robot, kg
         self.J = np.array([[76148072.89, 70089.52, 2067970.36],
                            [70089.52, 45477183.53, -87045.58],
-                           [2067970.36, -87045.58, 76287220.47]])*1000  # g/mm2 to kg/m2
-        self.r = np.array([0.02201854, 6.80044366, 0.97499173]) / 1000  # mm to m
-        rhat = hat(self.r)
+                           [2067970.36, -87045.58, 76287220.47]])*(10**(-9))  # g/mm2 to kg/m2
         Jinv = np.linalg.inv(self.J)
+        self.rh = np.array([0.02201854, 6.80044366, 0.97499173]) / 1000  # mm to m
         self.g = 9.807  # gravitational acceleration, m/s2
         self.t_p = 0.8  # gait period, seconds
         self.phi_switch = 0.5  # switching phase, must be between 0 and 1. Percentage of gait spent in contact.
-        self.N = 20  # mpc prediction horizon length (mpc steps)  # TODO: Modify
+        self.N = 40  # mpc prediction horizon length (mpc steps)  # TODO: Modify
         self.mpc_dt = 0.05  # mpc sampling time (s), needs to be a factor of N
         self.mpc_factor = int(self.mpc_dt / self.dt)  # mpc sampling time (timesteps), repeat mpc every x timesteps
         self.N_time = self.N * self.mpc_dt  # mpc horizon time
@@ -48,7 +47,7 @@ class Runner:
         elif tool == 'casadi':
             mpc_tool = mpc_euler_cas
 
-        self.mpc = mpc_tool.Mpc(t=self.mpc_dt, N=self.N, Jinv=Jinv, rhat=rhat, m=self.m, g=self.g, mu=mu)
+        self.mpc = mpc_tool.Mpc(t=self.mpc_dt, N=self.N, Jinv=Jinv, rh=self.rh, m=self.m, g=self.g, mu=mu)
         self.n_X = 13
         self.n_U = 6
 
@@ -64,9 +63,9 @@ class Runner:
         f_hist = np.zeros((total, self.n_U))
         s_hist = np.zeros(total)
         U = np.zeros(self.n_U)
-        pf_ref = np.zeros((total, self.n_U))
-        f_pred_hist = np.zeros((total, self.n_U))
-        p_pred_hist = np.zeros((total, self.n_U))
+        # pf_ref = np.zeros((total, self.n_U))
+        # f_pred_hist = np.zeros((total, self.n_U))
+        # p_pred_hist = np.zeros((total, self.n_U))
         for k in tqdm(range(0, self.total_run)):
             t = t + self.dt
 
@@ -78,7 +77,8 @@ class Runner:
                 x_in = convert(X_traj[k, :])  # convert to mpc states
                 x_ref = self.path_plan(x_in=x_in, xf=convert(self.X_f))
                 x_refN = x_ref[::int(mpc_factor)]
-                U = self.mpc.mpcontrol(x_in=x_in, x_ref_in=x_refN, C=C)
+                rf = np.array([0, 0, -0.4])  # body frame foot position TODO: Needs to be legit in real setup
+                U = self.mpc.mpcontrol(x_in=x_in, x_ref_in=x_refN, rf=rf, C=C)
 
             mpc_counter += 1
             f_hist[k, :] = U  # * s  # take first timestep
@@ -90,7 +90,7 @@ class Runner:
         # plots.posplot(p_ref=self.X_f[0:3], p_hist=X_traj[:, 0:3],
         #   p_pred_hist=p_pred_hist, f_pred_hist=f_pred_hist, pf_hist=pf_ref)
         plots.posplot_animate(p_ref=self.X_f[0:3], p_hist=X_traj[::50, 0:3])
-        # plots.posplot_animate_cube(p_ref=self.X_f[0:3], X_hist=X_traj[::50, :])
+        plots.posplot_animate_cube(p_ref=self.X_f[0:3], X_hist=X_traj[::50, :])
 
         return None
 