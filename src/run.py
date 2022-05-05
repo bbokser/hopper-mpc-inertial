4a635e65c780b22c9b57b7e76cd8c671d499a215
bbokser <bbokser@gmail.com>
2022-05-05 10:40:51 -0400

    Removed unnecessary args, corrected starting height

--- src/run.py
+++ src/run.py
@@ -10,19 +10,11 @@ parser.add_argument("tool", help="'choose cvxpy or casadi",
                     choices=['cvxpy', 'casadi'],
                     type=str)
 
-parser.add_argument("dyn", help="'choose euler or se3",
-                    choices=['euler', 'se3'],
-                    type=str)
-
-parser.add_argument("ctrl", help="'choose closed-loop (mpc) or open-loop (trajopt)",
-                    choices=['closed', 'open'],
-                    type=str)
-
 parser.add_argument("--runtime", help="sim run time in ms (integer)", type=int, default=5000)
 
 args = parser.parse_args()
 
 dt = 1e-3
 
-runner = Runner(dt=dt, tool=args.tool, dyn=args.dyn, ctrl=args.ctrl, t_run=args.runtime)
+runner = Runner(dt=dt, tool=args.tool, t_run=args.runtime)
 runner.run()
